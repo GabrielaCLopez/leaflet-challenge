c9a540facb8347983fac31d16267c7af3d34ad0b
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-04-02 19:15:21 -0700

    created the base map

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,12 +1,85 @@
 // Store our API endpoint as queryUrl.
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// Create the map
+var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    //layers: [street, earthquakes]
+  });
+  
+  // Adding tile layers
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+  L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  }).addTo(myMap);
+
+
+
+
 // Perform a GET request to the query URL/
-d3.json(queryUrl).then((data)=>console.log(data));
-// .then(function (data) {
-//   // Once we get a response, send the data.features object to the createFeatures function.
-//   createFeatures(data.features);
-// });
+d3.json(queryUrl).then(function(data) {
+    // console.log(data)
+
+    // Plot all the earthquakes from your dataset based on their longitude and latitude
+    var earthquakes = L.layerGroup();
+
+
+
+    // Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. 
+    // Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+    // Hint: The depth of the earth can be found as the third coordinate for each earthquake.
+
+
+    // Include popups that provide additional information about the earthquake when its associated marker is clicked.
+
+
+
+
+
+    // Create a legend that will provide context for your map data
+
+});
+
+
+
+// Create the map
+function createMap(earthquakes){ 
+
+
+    // Create a baseMaps object.
+    var baseMaps = {
+      "Street Map": street,
+      "Topographic Map": topo
+    };
+  
+    // Create an overlay object to hold our overlay.
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function createFeatures(earthquakeData) {
 
@@ -26,42 +99,11 @@ d3.json(queryUrl).then((data)=>console.log(data));
 //   createMap(earthquakes);
 // }
 
-// function createMap(earthquakes) {
 
-//   // Create the base layers.
-//   var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   })
-
-//   var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-//   });
-
-//   // Create a baseMaps object.
-//   var baseMaps = {
-//     "Street Map": street,
-//     "Topographic Map": topo
-//   };
-
-//   // Create an overlay object to hold our overlay.
-//   var overlayMaps = {
-//     Earthquakes: earthquakes
-//   };
-
-//   // Create our map, giving it the streetmap and earthquakes layers to display on load.
-//   var myMap = L.map("map", {
-//     center: [
-//       37.09, -95.71
-//     ],
-//     zoom: 5,
-//     layers: [street, earthquakes]
-//   });
 
 //   // Create a layer control.
 //   // Pass it our baseMaps and overlayMaps.
-//   // Add the layer control to the map.
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
-
-// }
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+ 
+} 
