cc0ce58027fc7c444d76db8320f0693c6678126c
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-04-09 18:10:10 -0700

    working on legend

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,6 +1,7 @@
 // Store our API endpoint as queryUrl.
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+
 // Create the map
 var myMap = L.map("map", {
     center: [
@@ -30,37 +31,82 @@ d3.json(queryUrl).then(function(data){
 // Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. 
 // Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
 // Hint: The depth of the earth can be found as the third coordinate for each earthquake.
-
- 
-
-
-// Include popups that provide additional information about the earthquake when its associated marker is clicked.
 function createFeatures(earthquakeData) {
 
   // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
+  // Give each feature a popup that describes the place, time, magnitude and depth of the earthquake.
   function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+
+  // Define a function to set the color of the marker based on the depth of the earthquake.
+  function getColor(depth) {
+    if (depth >=90) {
+      return '#C0392B';
+    }
+    else if (depth >70){
+      return '#EC7C3C';
+    }
+    else if (depth > 50) {
+      return '#F39C12';
+    }
+    else if (depth > 30){
+      return '#F1C40F';
+    }
+    else if (depth > 10){
+      return '#2ECC71';
+    }
+    else {
+      return '#1E8449';
+    }
+
+  }
+
+  // Define a function to set the radius of the marker based on the magnitude of the earthquake.
+  function getRadius(magnitude) {
+    return magnitude * 4;
   }
 
   // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
+  // Use pointToLayer option to create circle markers, and set their size and color based on the magnitude and depth.
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: getRadius(feature.properties.mag),
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: '#000',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+    }
   }).addTo(myMap);
 
+// Create a legend to provide context for the map data.
+var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var depths = [-10, 10, 30,50, 70, 90];
+    var colors = ['#C0392B', '#EC7C3C','#F39C12','#F1C40F','#2ECC71', '#1E8449']
+
+  //   // Add the minimum and maximum.
+  //   var legendInfo = "<h1>Population with Children<br />(ages 6-17)</h1>" +
+  //     "<div class=\"labels\">" +
+  //       "<div class=\"min\">" + limits[0] + "</div>" +
+  //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //     "</div>";
+
+  //   div.innerHTML = legendInfo;
+
+  //   limits.forEach(function(limit, index) {
+  //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  //   });
+
+  //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //   return div;
+  // };
+
+  // Adding the legend to the map
+  legend.addTo(myMap);
 }
-
-// Create a legend that will provide context for your map data
-
-
-
-
-
-
-
-
-//   // Create a layer control.
-//   // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-//   L.control.layers(baseMaps, overlayMaps).addTo(myMap);
